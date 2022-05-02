f9faf3d4dbf57031b2109829700ccd044c0e0038
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-02 08:53:51 +0200

    created main component

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,15 @@ import ReactDOM from 'react-dom';
 // Bundles './index.scss' file
 import './index.scss';
 
+// Main component 
+class myFlixApplication extends React.Component {
+    render() {
+        return (
+            <div className="my-flix">
+                <div>Good morning</div>
+            </div>
+        );
+    }
+}
+
 
