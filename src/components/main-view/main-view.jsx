dded4d769c8fa825e4dceab54d5d0e405d1a22c9
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-02 13:48:16 +0200

    Created MainView component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+export class MainView extends React.Component {
+
+    render() {
+        return (
+            <div className="main-view">
+                <div>Suspiria</div>
+                <div>The Wizard of Oz</div>
+                <div>Logan's Run</div>
+            </div>
+        );
+    }
+}
