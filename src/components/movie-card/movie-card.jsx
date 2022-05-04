3d781e16f72be8e5aa9f5b8bc07569538fa57a58
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-04 12:42:58 +0200

    Moved to new line for easier reading

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export class MovieCard extends React.Component {
     render() {
         const { movie } = this.props; // Extracts MovieCard data
-        return <div className="movie-card" onClick={() => { this.state.selectedMovie = movie; }}>{movie.Title}</div>;
+        return <div className="movie-card" onClick={() => { 
+            this.state.selectedMovie = movie; }}>{movie.Title}</div>;
     }
 }
