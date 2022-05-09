45a64040d7c8ad046d9a7dd323926e6a23977265
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-09 08:31:41 +0200

    Added return statement for login input fields

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -12,4 +12,18 @@ export function LoginView(props) {
         // then call props.onLoggenIn(username)
         //props.onLoggedIn(username);
     };
+
+    return (
+        <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+        </form>
+    );
 }
