130281d59086139503eeb4daa5b8e3fee9c4ab49
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-09 10:25:53 +0200

    Changed value name

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -8,7 +8,7 @@ export function RegistrationView(props) {
     const [ username, setUsername] = useState('');
     const [ password, setPassword ] = useState('');
     const [ email, setEmail ] = useState('');
-    const [ date, setBirthday ] = useState('');
+    const [ birthday, setBirthday ] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -34,7 +34,7 @@ export function RegistrationView(props) {
             </label>
             <label>
                 Birthday:
-                <input type="date" value={date} onChange={e => setBirthday(e.target.value)} />
+                <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
             </label>
             <button type="submit" onClick={handleSubmit}>Submit</button>
         </form>
