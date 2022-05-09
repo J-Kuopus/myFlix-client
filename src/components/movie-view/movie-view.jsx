29e80a97267b5b87b9de97924c62053a7fac4d35
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-09 11:10:30 +0200

    Defined PropTypes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -34,3 +34,20 @@ export class MovieView extends React.Component {
         );
     }
 }
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            BirthYear: PropTypes.number.isRequired
+        }),
+    }).isRequired,
+};
