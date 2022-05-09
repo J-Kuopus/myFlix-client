a36ec73bd37ab4a064d17203caf2ac0e8576d3ba
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-09 09:03:21 +0200

    Uncommented code, connects to MainView

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -10,7 +10,7 @@ export function LoginView(props) {
         console.log(username, password);
         // Send a request to the server for authentication
         // then call props.onLoggenIn(username)
-        //props.onLoggedIn(username);
+        props.onLoggedIn(username);
     };
 
     return (
