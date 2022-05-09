bbcb6f2c44b96020d35fa4173eddb88454e3d7e6
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-09 08:26:03 +0200

    Created LoginView function component

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,2 +1,15 @@
 // Implements useState Hook
 import React, { useState } from 'react';
+
+export function LoginView(props) {
+    const [ username, setUsername] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        // Send a request to the server for authentication
+        // then call props.onLoggenIn(username)
+        //props.onLoggedIn(username);
+    };
+}
