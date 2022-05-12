65b6f4f8000218c75c8d80c257c89b79ec6d8eb6
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-12 08:39:27 +0200

    Wrapped form in react-bootstrap form components

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -20,52 +20,62 @@ export function RegistrationView(props) {
     };
 
     return (
-        <Form>
-            <Form.Group>
-            <Form.Label>Username: </Form.Label>
-            <Form.Control
-                type="text" 
-                value={username} 
-                onChange={e => setUsername(e.target.value)}
-                required
-                placeholder="Enter a username"
-            />
-            </Form.Group>
+        <Container>
+            <Row>
+            <Col>
+            <CardGroup>
+                <Card>
+                    <Form>
+                        <Form.Group>
+                        <Form.Label>Username: </Form.Label>
+                        <Form.Control
+                            type="text" 
+                            value={username} 
+                            onChange={e => setUsername(e.target.value)}
+                            required
+                            placeholder="Enter a username"
+                        />
+                        </Form.Group>
 
-            <Form.Group>
-            <Form.Label>Password: </Form.Label>
-            <Form.Control 
-                type="password" 
-                value={password} 
-                onChange={e => setPassword(e.target.value)}
-                required
-            />
-            </Form.Group>
+                        <Form.Group>
+                        <Form.Label>Password: </Form.Label>
+                        <Form.Control 
+                            type="password" 
+                            value={password} 
+                            onChange={e => setPassword(e.target.value)}
+                            required
+                        />
+                        </Form.Group>
 
-            <Form.Group>
-            <Form.Label>Email: </Form.Label>
-            <Form.Control 
-                type="email" 
-                value={email} 
-                onChange={e => setEmail(e.target.value)}
-                required 
-            />
-            </Form.Group>
+                        <Form.Group>
+                        <Form.Label>Email: </Form.Label>
+                        <Form.Control 
+                            type="email" 
+                            value={email} 
+                            onChange={e => setEmail(e.target.value)}
+                            required 
+                        />
+                        </Form.Group>
 
-            <Form.Group>
-            <Form.Label>Birthday: </Form.Label>
-            <Form.Control 
-                type="date" 
-                value={birthday} 
-                onChange={e => setBirthday(e.target.value)} 
-            />
-            </Form.Group>
-            <Button 
-                variant="primary"
-                type="submit" 
-                onClick={handleSubmit}>Submit
-            </Button>
-        </Form>
+                        <Form.Group>
+                        <Form.Label>Birthday: </Form.Label>
+                        <Form.Control 
+                            type="date" 
+                            value={birthday} 
+                            onChange={e => setBirthday(e.target.value)} 
+                        />
+                        </Form.Group>
+                        <Button 
+                            variant="primary"
+                            type="submit" 
+                            onClick={handleSubmit}>Submit
+                        </Button>
+                    </Form>
+                </Card>
+            </CardGroup>
+            </Col>
+            </Row>
+        </Container>
     );
 }
 
