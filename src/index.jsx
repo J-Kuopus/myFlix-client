241fd53e5c9d3ae1a4ad98ab0f43d3ec82c0c259
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-13 14:13:41 +0200

    Increased logo width

--- src/index.jsx
+++ src/index.jsx
@@ -20,7 +20,7 @@ class MyFlixApplication extends React.Component {
                         <Navbar.Brand className="navbar-brand" href="#home">
                             <img
                                 src={image}
-                                width="60"
+                                width="70"
                                 className="navbar-logo d-inline-block align-top"
                                 alt="Retro Video logo"
                             />{' '}
