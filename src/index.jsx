af6493fc51a3ac90bff8be158c44739ec3b84371
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-14 12:17:13 +0200

    Decreased img width

--- src/index.jsx
+++ src/index.jsx
@@ -20,7 +20,7 @@ class MyFlixApplication extends React.Component {
                         <Navbar.Brand className="navbar-brand" href="#home">
                             <img
                                 src={image}
-                                width="70"
+                                width="65"
                                 className="navbar-logo d-inline-block align-top"
                                 alt="Retro Video logo"
                             />{' '}
