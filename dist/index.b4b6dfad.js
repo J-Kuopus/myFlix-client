7a29701b7aff60401f7c8365d9f8cfc062bea56c
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-19 12:54:59 +0200

    Changed amount of required characters

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -35590,7 +35590,7 @@ function LoginView(props) {
             setUsernameErr('Username Required');
             isReq = false;
         } else if (username.length < 2) {
-            setUsernameErr('Username must be at least 2 characters long');
+            setUsernameErr('Username must be at least 5 characters long');
             isReq = false;
         }
         if (!password) {
