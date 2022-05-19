8c16e26912193799b40d5cc212251257799df618
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-19 12:38:18 +0200

    Displays validation errors for input fields

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -86,6 +86,7 @@ export function RegistrationView(props) {
                             onChange={e => setUsername(e.target.value)}
                             placeholder="Enter a username"
                         />
+                        {values.usernameErr && <p>{values.usernameErr}</p>} {/* Displays validation error */}
                         </Form.Group>
 
                         <Form.Group>
@@ -95,7 +96,7 @@ export function RegistrationView(props) {
                             value={password} 
                             onChange={e => setPassword(e.target.value)}
                             placeholder="Enter a password"
-                        />
+                        />{values.passwordErr && <p>{values.passwordErr}</p>}
                         </Form.Group>
 
                         <Form.Group>
@@ -106,6 +107,7 @@ export function RegistrationView(props) {
                             onChange={e => setEmail(e.target.value)}
                             placeholder="Enter your email address"
                         />
+                        {values.emailErr && <p>{values.emailErr}</p>}
                         </Form.Group>
 
                         <Form.Group>
