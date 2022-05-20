39704a7f3a0915bd2630df1ec716f569b1cfc896
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-20 09:36:05 +0200

    Changed to function
    Changed this to a function instead of a component that stores token from local storage and displays username in navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,13 +5,31 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import './navbar.scss';
 import image from '../img/rv-logo.png';
 
-export class NavbarView extends React.Component {
-    render() {
+export function NavbarView() {
+    let user = localStorage.getItem('user');
+
+    const onLoggedOut = () => {
+        localStorage.clear();
+        window.open('/', '_self');
+    };
+
+    const isAuth = () => {
+        if (typeof window == 'undefined') {
+            return false;
+        }
+        if (localStorage.getItem('token')) {
+            return localStorage.getItem('token')
+        } else {
+            return false;
+        }
+    };
+
+
         return (
             <Container>
                 <Navbar className="navbar" fixed="top" bg="dark" variant="dark" expand="lg">
                     <Container>
-                        <Navbar.Brand className="navbar-brand" href="#home">
+                        <Navbar.Brand className="navbar-brand" href="/">
                             <img
                                 src={image}
                                 width="70"
@@ -22,15 +40,20 @@ export class NavbarView extends React.Component {
                         </Navbar.Brand>
                     </Container>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                    {isAuth() && (
                     <Navbar.Collapse>
                         <Nav className="me-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/">Hello, {user}</Nav.Link>
                             <Nav.Link href="#">Profile</Nav.Link>
-                            <Nav.Link onClick={() => { this.onLoggedOut() }}>Logout</Nav.Link>
+                            <Nav.Link onClick={() => { 
+                                onLoggedOut(); 
+                            }}>Logout
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
+                    )}
                 </Navbar>
             </Container>
         );
-    }
 }
