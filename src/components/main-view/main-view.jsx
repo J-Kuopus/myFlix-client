ef7431cd3747c2a4fab939fb4893d91fe313e7b1
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-20 09:40:15 +0200

    Imported profile view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -10,6 +10,7 @@ import { MovieView } from '../movie-view/movie-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
+import { ProfileView } from '../profile-view/profile-view';
 import NavbarView from '../navbar/navbar';
 
 class MainView extends React.Component {
