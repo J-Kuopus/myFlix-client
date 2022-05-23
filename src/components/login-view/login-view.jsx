de6e0d10f5cdc98fbaf50d4c7abf486d2f0366ed
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-23 08:53:50 +0200

    Imported code from dev branch

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,25 +2,62 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import './login-view.scss';
 
 export function LoginView(props) {
     const [ username, setUsername] = useState('');
     const [ password, setPassword ] = useState('');
+    // Declare hook for each input
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
+
+    // Validate user inputs
+    const validate = () => {
+        let isReq = true;
+        if(!username){
+            setUsernameErr('Username Required');
+            isReq = false;
+        }else if(username.length < 2){
+            setUsernameErr('Username must be at least 5 characters long');
+            isReq = false;
+        }
+        if(!password){
+            setPasswordErr('Password Required');
+            isReq = false;
+        }else if(password.length < 6){
+            setPasswordErr('Password must be at least 6 characters long');
+            isReq = false;
+        }
+        return isReq;
+        
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        // Send a request to the server for authentication
-        // then call props.onLoggedIn(username)
-        props.onLoggedIn(username);
+        const isReq = validate();
+        if(isReq) {
+        //Send a request to the server for authentication
+          axios.post('https://powerful-coast-48240.herokuapp.com/login', {
+            Username: username,
+            Password: password
+          })
+          .then(response => {
+            const data = response.data;
+            props.onLoggedIn(data);
+          })
+          .catch(e => {
+            console.log('No such user.')
+          });
+        }
     };
-
+    
     return (
         <Container>
             <Row className="justify-content-center">
-            <Col lg={5}>
+            <Col>
             <CardGroup>
                 <Card className="login-card">
                     <Card.Body>
@@ -30,17 +67,23 @@ export function LoginView(props) {
                         <Form.Group controlId="formUsername">
                             <Form.Label>Username:</Form.Label>
                             <Form.Control 
-                                type ="text" 
+                                type ="text"
+                                value={username} 
                                 onChange={e => setUsername(e.target.value)} 
                             />
+                            {/* Displays validation error */}
+                            {usernameErr && <p>{usernameErr}</p>}
                         </Form.Group>
                                 
                         <Form.Group controlId="formPassword">
                             <Form.Label>Password:</Form.Label>
                                 <Form.Control 
-                                    type="password" 
+                                    type="password"
+                                    value={password} 
                                     onChange={ e => setPassword(e.target.value)} 
                                 />
+                                {/* Displays validation error */}
+                                {passwordErr && <p>{passwordErr}</p>}
                         </Form.Group>
                         <Button 
                             variant="danger" 
@@ -51,12 +94,14 @@ export function LoginView(props) {
                     </Card.Body>
                     <Card.Footer>
                         <Card.Text>Don't have an account? You can register here.</Card.Text>
+                        <Link to={'/register'}>
                         <Button 
-                            href="#"
                             variant="outline-secondary"
-                            size="sm">
+                            size="sm"
+                            type="submit">
                             Register
                         </Button>
+                        </Link>
                     </Card.Footer>
                 </Card>
             </CardGroup>
