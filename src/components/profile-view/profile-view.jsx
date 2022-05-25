4b71cd29b53e2b0cc70288462def4f7d94f42a8a
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-25 12:38:45 +0200

    Updated component method to render user data
    Also removed imports of other views

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -3,32 +3,57 @@ import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootst
 import { Link } from 'react-router-dom';
 import './profile-view.scss';
 import axios from 'axios';
-import UserInfo from './user-info';
-import UpdateUser from './update-user';
-import FavoriteMovies from './favorite-movies';
 
-export function ProfileView({ movies, onUpdatedUserInfo }) {
-    const [user, setUser] = useState({});
+export class ProfileView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: null,
+            password: null,
+            email: null,
+            birthday: null,
+            favoriteMovies: [],
+        };
+    };
+
+    getUser(token) {
+        let user = localStorage.getItem("user");
+        axios.get(`https://powerful-coast-48240.herokuapp.com/users/${user}`, {
+            headers: { Authorization: `Bearer ${token}` },
+          })
+          .then((response) => {
+            //assign the result to the state
+            this.setState({
+              username: response.data.username,
+              password: response.data.password,
+              email: response.data.email,
+              birthday: response.data.birthday,
+              favoriteMovies: response.data.favoriteMovies,
+            });
+          })
+          .catch((e) => console.log(e));
+      }
+
+      componentDidMount() {
+        const accessToken = localStorage.getItem("token");
+        this.getUser(accessToken);
+      };
+
+      render() {
+          const { username, password, email, birthday, favoriteMovies } = this.state;
     
-
-    const favoriteMovieList = movies.filter((movies) => {});
-
-    const getUser = () => {};
-
-    const handleSubmit = (e) => {};
-
-    const removeFav = (id) => {};
-
-    const handleUpdate = (e) => {};
-
-    return (
+      return (
         <Container>
-            <UserInfo name={user.Username} password={user.Password} email={user.Email} birthday={user.Birthday} />
-            <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-            <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
+            <Card>
+                <Card.Text>Username: {username}</Card.Text>
+                <Card.Text>Password: {password}</Card.Text>
+                <Card.Text>Email: {email}</Card.Text>
+                <Card.Text>Birthday: {birthday}</Card.Text>
+                <Card.Text>Favorite Movies: {favoriteMovies}</Card.Text>
+            </Card>
         </Container>
-    );
+        );
+    };
 
 }
 
-export default ProfileView;
