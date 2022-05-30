714bd197d7421420ca3c4b051ff6fc185deb76f1
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-05-30 14:05:17 +0200

    Created function for displaying favorite movies

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -4,26 +4,58 @@ import axios from 'axios';
 import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function FavoriteMovies({ favoriteMovieList }) {
-    return (
-        <Container>
-            <Card>
-                <Card.Text>Favorite Movies</Card.Text>
-                {favoriteMovieList.map((movies) => {
-                    return (
-                        <div key={movies._id}>
-                            <img src={movies.ImagePath} />
-                            <Link to={'movies/:MovieID'}>
-                                {movies.Title}
-                            </Link>
-                            <Button variant="secondary"
-                                    onClick={() => removeFav(movies._id)}>Remove from list</Button>
-                        </div>
-                    )
-                })};
-            </Card>
-        </Container>
-    )
-}
+export function FavoriteMoviesView(props) {
+    const { movies, favoriteMovies, currentUser, token } = props;
+  
+    const favoriteMoviesId = favoriteMovies.map(m => m._id)
+  
+    const favoriteMoviesList = movies.filter(m => {
+      return favoriteMoviesId.includes(m._id)
+    })
+  
 
-export default FavoriteMovies;
+const handleMovieDelete = (movieId) => {
+    axios.delete(`https://powerful-coast-48240.herokuapp.com/users/${currentUser}/movies/${movieId}`, {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(() => {
+      alert(`The movie was removed from favorites list.`)
+      window.open('/users/:Username', '_self');
+    }).
+    catch(error => console.error(error))
+
+  return (
+    <Container>
+      {favoriteMoviesList.length === 0 ? (
+          <p>You have no favorite movies yet.</p>
+          ) : (
+            favoriteMoviesList.map((movie) => {
+              return (
+              <Col xs={10} sm={8} md={6} lg={4} >
+                <Card id="movie-card">
+                  <Link to={`/movies/${movie._id}`}>
+                    <Card.Img variant="top" src={movie.ImagePath} />
+                  </Link>
+                  <Card.Body>
+                    <Card.Title>{movie.Title}</Card.Title>
+                    <Card.Text>{movie.Description}</Card.Text>
+                    <Link to={`/movies/${movie._id}`}>
+                      <Button className="button" variant="outline-primary" size="sm">Open</Button>
+                    </Link>
+                    <Button 
+                    className="button ml-2" 
+                    variant="outline-primary" 
+                    size="sm" onClick={()=> {handleMovieDelete(movie._id)}} >
+                      Remove
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              )
+            })
+          )
+        }
+    </Container>
+  )
+}
+}
