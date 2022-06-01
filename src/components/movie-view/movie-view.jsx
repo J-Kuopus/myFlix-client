d58e4fe9a0d4a6e4bd7509a8e59bae15b94d195a
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-01 10:33:42 +0200

    Removed add movie to favorites function

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,21 +8,6 @@ import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
-    // PUTS movie on favorite list
-    addToFavoriteList(movieId) {
-        const currentUser = localStorage.getItem('user');
-        const token = localStorage.getItem('token');
-        axios.put(`https://powerful-coast-48240.herokuapp.com/users/${currentUser}/movies/${movieId}`, 
-        {},
-        {
-          headers: { Authorization: `Bearer ${token}`}
-        })
-        .then((response) => {
-          console.log(response.data)
-          alert(`The movie was added to your favorites.`)
-        }).
-        catch(error => console.error(error))
-      }
 
     render() {
         const { movie, onBackClick } = this.props;
@@ -42,7 +27,7 @@ export class MovieView extends React.Component {
                                 <span className="label">Released: </span>{movie.Released}</ListGroup.Item>
                             <ListGroup.Item><span className="label">Summary: </span>{movie.Description}</ListGroup.Item> 
                         </ListGroup>
-                        <Button  variant="primary" size="sm" onClick={() => this.addToFavoriteList(movie._id) }>Add to favorites</Button>
+                        <Button  variant="primary" size="sm" onClick={() => this.addToFavorites(movie._id) }>Add to favorites</Button>
                     </div> 
                     </Col>
                     <Col xxl="4" xl="4" lg="6" md="4">
