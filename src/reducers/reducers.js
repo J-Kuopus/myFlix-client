364d703af2455a95a3eae4db5aea83692eee8928
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-04 14:29:59 +0200

    Created reducers for the other states

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+import { SET_FILTER, 
+         SET_MOVIES,
+         SET_USER,
+         ADD_FAV,
+         REM_FAV 
+        } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
     switch (action.type) {
@@ -20,9 +25,23 @@ function movies(state = [], action) {
     }
 }
 
+function user(state = '', action) {
+    switch (action.type) {
+        case SET_USER:
+            return action.user || localStorage.getItem('user') || '';
+        case ADD_FAV:
+            return action.value;
+        case REM_FAV:
+            return action.value;
+        default:
+            return state;
+    }
+}
+
 const moviesApp = combineReducers({
     visibilityFilter,
-    movies
+    movies,
+    user
 });
 
 export default moviesApp;
