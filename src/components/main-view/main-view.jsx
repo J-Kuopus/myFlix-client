87bf000626b6261487f072f84b5c72d3b0ed31ad
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-05 15:24:34 +0200

    Removed curly braces

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -13,7 +13,7 @@ import { MovieView } from '../movie-view/movie-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
-import { ProfileView } from '../profile-view/profile-view';
+import  ProfileView  from '../profile-view/profile-view';
 import { NavbarView } from '../navbar/navbar';
 
 class MainView extends React.Component {
