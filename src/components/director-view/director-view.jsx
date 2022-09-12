49e41a115ea00ae2e514c21197cc13adfd5beedf
J-Kuopus <kuopus79@gmail.com>
2022-09-12 12:45:57 +0100

    Removed Row and Col imports

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export class DirectorView extends React.Component {
