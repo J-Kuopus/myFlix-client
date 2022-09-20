22c6e49c91a235cb4ccd5905e3020ee99f16533f
J-Kuopus <kuopus79@gmail.com>
2022-09-20 16:00:57 +0100

    Removed unneeded elements

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card, Container, ListGroup } from 'react-bootstrap';
+import { Button, Card, Container, ListGroup, Col, Row } from 'react-bootstrap';
 import { IoArrowBackCircleSharp } from "react-icons/io5";
 import './director-view.scss';
 
@@ -9,24 +9,29 @@ export class DirectorView extends React.Component {
         const { director, onBackClick, movies } = this.props;
 
         return (
-            <Card>
+            <Container>
+            <IoArrowBackCircleSharp className='back-arrow-director' onClick={onBackClick}/> 
+            <Card className='director-info' style={{ backgroundColor:'#333'}}>
                 <Card.Body>
-                    <IoArrowBackCircleSharp className='back-arrow-director' onClick={() => { onBackClick(); }}/> 
-                    <p></p>
-                    <Container>
-                        <ListGroup>
-                            <ListGroup.Item><span className="director-label">Director Info</span></ListGroup.Item>
-                            <ListGroup.Item><span className="label">Name: </span>{director.Name}</ListGroup.Item>
-                            <ListGroup.Item><span className="label">Bio:  </span>{director.Bio}</ListGroup.Item>
-                            <ListGroup.Item><span className="label">Birthyear: </span>{director.Birth}</ListGroup.Item>
-                            <ListGroup.Item><span className="label">Deathyear: </span>{director.Death}</ListGroup.Item>
-                        </ListGroup>
-                    </Container>
-                    <p></p>
-                    <Container>
-                    </Container>
+                    <Row>
+                        <Col>
+                            <ListGroup className="dir-info-list">
+                                <ListGroup.Item><span className="director-label">Director Info</span></ListGroup.Item>
+                                <ListGroup.Item><span className="label">Name: </span>{director.Name}</ListGroup.Item>
+                                <ListGroup.Item><span className="label">Bio:  </span>{director.Bio}</ListGroup.Item>
+                                <ListGroup.Item><span className="label">Birthyear: </span>{director.Birth}</ListGroup.Item>
+                                <ListGroup.Item><span className="label">Deathyear: </span>{director.Death}</ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                        <Col>
+                            <div className='dir-img-wrapper'>
+                                <img className="director-photo" src={director.Photo} />
+                            </div>
+                        </Col>
+                    </Row>
                 </Card.Body>
             </Card>
+            </Container>
         )
     }
 }
